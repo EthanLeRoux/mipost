926863c2b96a856298b66237ecece738d20168e2
20261664 <20261664@ctucareer.co.za>
2026-03-11 13:39:59 +0200

    Remove OpenAPI configuration from development environment

--- Program.cs
+++ Program.cs
@@ -16,14 +16,12 @@ builder.Services.AddScoped<PostService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
